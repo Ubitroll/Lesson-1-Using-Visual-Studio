fc1b9585675d9afb510988ec1209fcdc9447fc96
Ubitroll <stephencartner@hotmail.co.uk>
2023-02-03 10:57:38 +0000

    ForEach

---
+++ ForEach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 2, 4, 6, 8, 10 };
+
+            foreach (int i in number)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
